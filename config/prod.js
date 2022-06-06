a02e9f24bd4bc14168973a4a06af461a43b8c76d
TamirGeron <tamir1234432@gmail.com>
2022-06-06 03:03:42 +0300

    make it better

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://Tamir:tamir123@cluster0.mvu5h.mongodb.net/?retryWrites=true&w=majority',
 }
